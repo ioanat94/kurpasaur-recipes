23e82e608d0749e0ba1499c7c152333ba4cee189
Ioana Tiplea <ioanatiplea94@gmail.com>
2024-12-25 08:47:15 +0200

    Create components and layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="grid grid-cols-[200px_1fr] bg-primary text-primary-foreground min-h-screen">
+	<Button class="w-max m-2" aria-label="Home"
+		><a href="/"><img src="/home.png" width="24px" alt="Home icon" /></a></Button
+	>
+
+	<div>
+		{@render children()}
+	</div>
+</div>
